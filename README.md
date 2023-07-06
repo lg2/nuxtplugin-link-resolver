40ebd57e72f7b45d7a6ae559f46afec7e30f5f32
Romain Avalle <romain.avalle@lg2.com>
2023-07-06 13:55:28 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# nuxtplugin-link-resolver
+
+create a `linkResolver`base on your ``~/assets/jsons/link-resolver.json`
+
+```
+$lr({__typename:'ArticleRecord'})
+```
---
+++ index.js
@@ -0,0 +1,18 @@
+import linkResolverJSON from "~/assets/jsons/link-resolver.json";
+const linkResolver = (data) => {
+  let route = { ...linkResolverJSON[data.__typename] };
+  if (route) {
+    return route;
+  } else {
+    console.warn(
+      "no route for : ",
+      data,
+      "in route",
+      process.client && window.location.href
+    );
+    return {};
+  }
+};
+export default ({ app }, inject) => {
+  inject("lr", linkResolver);
+};
