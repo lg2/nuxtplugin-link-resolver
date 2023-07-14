f49e7cddafdbc3f5524daee5750adc1e0545a176
Romain Avalle <romain.avalle@lg2.com>
2023-07-14 11:46:52 -0400

    add routes params

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const linkResolverJSON = require("./../../../assets/jsons/link-resolver.json")
 const linkResolver = function(data)  {
   let route = { ...linkResolverJSON[data.__typename] };
   if (route) {
+    if (route.params) {
+      const params = {}
+      Object.entries(route.params).forEach((param) => {
+        params[param[0]] = data[param[1]]
+      })
+      route.params = params
+    }
     return route;
   } else {
     console.warn(
